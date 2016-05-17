5ac929cb578fc70c6f1373d9d3bc618912fb4e92
Robin <rbechtel.ostmann@googlemail.com>
2016-05-16 20:33:09 -0400

    added a2

---
+++ a2.asm
@@ -0,0 +1,98 @@
+SECTION .data
+msg_input DB "F(n) | n=",0
+msg_input_format DB "%d",0
+msg_output DB "F(%d)=%lu",10,0
+
+SECTION .bss
+ergebnis RESD 2
+n RESD 1
+curr RESD 2
+last RESD 2
+
+SECTION .text
+global _start
+extern printf
+extern scanf
+
+_start:
+push ebp
+mov ebp, esp
+
+	push msg_input
+	call printf
+	add esp, 0x4 * 0x1
+
+	push n
+	push msg_input_format
+	call scanf
+	add esp, 0x4 * 0x2
+
+	push dword [n]
+	call fib
+	add esp, 0x4 * 0x1
+
+	push dword [ergebnis]
+	push dword [ergebnis + 0x4]
+	push dword [n]
+	push msg_output
+	call printf
+	add esp, 0x4 * 0x4
+
+leave
+ret
+
+fib:;(int n) returns: long ergebnis
+push ebp
+mov ebp, esp
+pusha
+
+	mov dword [ergebnis], 0x0
+	mov dword [ergebnis + 0x4], 0x0
+	mov dword [curr], 0x1
+	mov dword [curr + 0x4], 0x0
+	mov dword [last], 0x1
+	mov dword [last + 0x4], 0x0
+
+	mov eax, dword [ebp + 0x8]
+	mov ecx, 0x3
+
+	cmp eax, 0x0				; n < 0 ? invalid arg -> ergebnis = 0
+	JL fib_OV
+	cmp eax, 0x2				; n <= 2 ? ergebnis = (n+1)>> 1 : calc
+	JG fib_calc
+	inc ecx
+	shr ecx, 1
+	mov dword [ergebnis], 0x0
+	mov dword [ergebnis + 0x4], eax
+	JMP fib_end
+
+fib_calc:
+	mov eax, dword [curr]
+	add eax, dword [last]
+	mov dword [ergebnis], eax
+	mov eax, dword [curr + 0x4]
+	adc eax, dword [last + 0x4]
+	mov dword [ergebnis + 0x4], eax
+	JO fib_OV					;Overflow: ergebnis exceeds 64bit range
+	mov eax, dword [curr]
+	mov dword [last], eax
+	mov eax, dword [curr + 0x4]
+	mov dword[last + 0x4], eax
+	mov eax, dword [ergebnis]
+	mov dword[curr], eax
+	mov eax, dword[ergebnis + 0x4]
+	mov dword[curr + 0x4], eax
+
+	cmp ecx, eax 					;loop header
+	inc ecx
+	JLE fib_calc
+	JMP fib_end
+fib_OV:
+	mov dword [ergebnis], 0x0
+	mov dword [ergebnis + 0x4], 0x0
+	JMP fib_end
+
+fib_end:
+popa
+leave
+ret
--- makefile
+++ makefile
@@ -3,12 +3,16 @@ asm_flags = -f elf -F dwarf -g
 ld = ld
 ld_flags = -lc -I/lib/ld-linux.so.2
 
-all: a1
+all: a1 a2
 	make clean
 
 a1: a1.asm
 	${asm} ${asm_flags} a1.asm
 	${ld} ${ld_flags} -o a1 a1.o
+
+a2: a2.asm
+	${asm} ${asm_flags} a2.asm
+	${ld} ${ld_flags} -o a2 a2.o
 
 clean:
 	rm -f *.o
